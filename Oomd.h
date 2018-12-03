a351847d1428b304d00c1b832321b21178a67512
Daniel Xu <dlxu@fb.com>
2018-12-03 14:11:29 -0800

    Hook up new oomd2 engine to core oomd
    Summary:
    This patch hooks up the new oomd2 core to Main.cpp. At this point, we are
    not running the default OomKiller and OomDetector. All of oomd is nooping.
    The next few patches will migrate all the existing logic to oomd2.
    
    Changes:
    
    * Deleted Config.{h,cpp}. Oomd::Config2 (located in config/) replaces it
    * `Oomd` (the object) no longer uses OomDetector, OomKiller, or Tunables
      directly. The plugin architecture takes over that responsibility
    * Removed verbose printing. Nobody was using this
    
    Reviewed By: mokomull
    
    Differential Revision: D9662227
    
    fbshipit-source-id: 92cd7e7d0f8a9d9193c4d8da6a02e05cd4313239

--- Oomd.h
+++ Oomd.h
@@ -24,59 +24,26 @@
 #include <utility>
 #include <vector>
 
-#include "oomd/OomDetector.h"
-#include "oomd/OomKiller.h"
-#include "oomd/shared/KillList.h"
-#include "oomd/shared/Tunables.h"
+#include "oomd/engine/Engine.h"
 
 namespace Oomd {
 
-struct Cgroup {
-  Cgroup(std::unique_ptr<OomDetector> d, std::unique_ptr<OomKiller> k) {
-    detector = std::move(d);
-    killer = std::move(k);
-  }
-
-  std::unique_ptr<OomDetector> detector;
-  std::unique_ptr<OomKiller> killer;
-  OomdContext context;
-};
-
 class Oomd {
  public:
-  Oomd() = default;
+  Oomd(std::unique_ptr<Engine::Engine> engine, int interval);
   virtual ~Oomd() = default;
-  int run();
 
-  virtual void setVerbose(bool b = true) {
-    verbose_ = b;
-  }
-  virtual void setTunables(std::shared_ptr<Tunables> t) {
-    tunables_ = std::move(t);
-  }
-  virtual void addCgroup(
-      std::unique_ptr<OomDetector> d,
-      std::unique_ptr<OomKiller> k) {
-    cgroups_.emplace_back(std::make_unique<Cgroup>(std::move(d), std::move(k)));
-  }
-
-  bool prepareRun();
-  bool registerHandlers() const;
-  void updateTunables();
   void updateContext(const std::string& cgroup_path, OomdContext& ctx);
+  int run();
 
  private:
   // runtime settings
   std::chrono::seconds interval_{0};
-  std::chrono::seconds post_kill_delay_{0};
-  double average_size_decay_{0};
-  int verbose_ticks_{60};
+  const double average_size_decay_{
+      4}; // TODO(dlxu): migrate to ring buffer for raw datapoints so plugins
+          // can calculate weighted average themselves
 
-  // config args
-  bool verbose_{false};
-  std::shared_ptr<Tunables> tunables_;
-  std::vector<std::unique_ptr<Cgroup>> cgroups_;
-
+  std::unique_ptr<Engine::Engine> engine_;
   bool warned_io_pressure_{false};
 };
 
