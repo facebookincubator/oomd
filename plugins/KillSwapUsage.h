e4033d51502c00edd2e1aa33e29ab40dc6f57aae
Daniel Xu <dlxu@fb.com>
2018-12-03 13:01:10 -0800

    Implement kill_by_swap_usage plugin
    Summary: This is part of the OomKiller reimplementation
    
    Differential Revision: D9840774
    
    fbshipit-source-id: 2a2719f256daa8b384189f6b597d5e2067bd61bb

---
+++ plugins/KillSwapUsage.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018-present, Facebook, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#pragma once
+
+#include <chrono>
+
+#include "oomd/plugins/BaseKillPlugin.h"
+
+namespace Oomd {
+
+template <typename Base = BaseKillPlugin>
+class KillSwapUsage : public Base {
+ public:
+  int init(
+      Engine::MonitoredResources& resources,
+      std::unordered_map<std::string, std::string> args) override;
+
+  Engine::PluginRet run(OomdContext& ctx) override;
+
+  static KillSwapUsage* create() {
+    return new KillSwapUsage();
+  }
+
+  ~KillSwapUsage() = default;
+
+ protected:
+  virtual bool tryToKillSomething(OomdContext& ctx);
+
+  std::string cgroup_;
+  std::string cgroup_fs_;
+  int post_action_delay_{15};
+  bool dry_{false};
+
+ private:
+  static constexpr auto kCgroupFs = "/sys/fs/cgroup";
+};
+
+} // namespace Oomd
+
+#include "oomd/plugins/KillSwapUsage-inl.h"
