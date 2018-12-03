77334a8203791fe488c8a96a60cbd5e2822c9dd2
Daniel Xu <dlxu@fb.com>
2018-12-03 13:01:12 -0800

    Add wildcard support to detector plugins
    Summary:
    This patch adds wildcarded cgroup path support to the core
    detector plugins. This is useful for monitoring cgroups where the
    full name may not be known at oomd configuration time.
    
    Note we only keep track of the highest memory pressure cgroup if
    the wildcarded path globs to more than one cgroup. The reason behind
    this is two fold:
    
        * We expect the common case of wildcard usage to only glob to
          one cgroup. If globbing to multiple cgroups is desired, it would
          make more sense to construct the wildcard paths such that every
          glob only maps to one cgroup.
    
        * In the event that the wildcard path globs to more than one
          cgroup, keeping track of the max([pressure(cgroup) for cgroup
          in globbed]) gives equal representation to each cgroup.
    
    Differential Revision: D13087317
    
    fbshipit-source-id: 898e25835e2506e4c224c9a211b8a01c2279f3f7

--- plugins/PressureAbove.cpp
+++ plugins/PressureAbove.cpp
@@ -78,20 +78,35 @@ int PressureAbove::init(
 Engine::PluginRet PressureAbove::run(OomdContext& /* unused */) {
   using std::chrono::steady_clock;
 
+  auto resolved = Fs::resolveWildcardPath(cgroup_fs_ + "/" + cgroup_);
   ResourcePressure current_pressure;
-  switch (resource_) {
-    case ResourceType::IO:
-      current_pressure = Fs::readIopressure(cgroup_fs_ + "/" + cgroup_);
-      break;
-    case ResourceType::MEMORY:
-      current_pressure = Fs::readMempressure(cgroup_fs_ + "/" + cgroup_);
-      break;
+  int64_t current_memory_usage = 0;
+
+  for (const auto& abs_cgroup_path : resolved) {
+    ResourcePressure rp;
+    switch (resource_) {
+      case ResourceType::IO:
+        rp = Fs::readIopressure(abs_cgroup_path);
+        break;
+      case ResourceType::MEMORY:
+        rp = Fs::readMempressure(abs_cgroup_path);
+        break;
+        // No default to catch new additions in ResourceType
+    }
+
+    // Do a weighted comparison (we care more about 10s, then 60s, then 600s)
+    if (rp.sec_10 * 3 + rp.sec_60 * 2 + rp.sec_600 >
+        current_pressure.sec_10 * 3 + current_pressure.sec_60 * 2 +
+            current_pressure.sec_600) {
+      current_pressure = rp;
+      current_memory_usage = Fs::readMemcurrent(abs_cgroup_path);
+    }
   }
+
   OOMD_SCOPE_EXIT {
     last_pressure_ = current_pressure;
   };
 
-  int64_t current_memory_usage = Fs::readMemcurrent(cgroup_fs_ + "/" + cgroup_);
   const auto now = steady_clock::now();
 
   // Check if the 10s pressure is above threshold_ for duration_
