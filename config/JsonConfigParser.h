923c59f98961bfdef34d3a022ca553e1ca4af5d1
Daniel Xu <dlxu@fb.com>
2018-12-03 13:01:43 -0800

    Add JSON parsing class
    Summary:
    JsonConfigParser will parse JSON and create an intermediate
    representation (IR) of the config. Later on, a Config class will ingest
    the IR and instantiate everything.
    
    Differential Revision: D9375008
    
    fbshipit-source-id: 2fdf6fcc29d4442030a3185e171d912364fcf6de

---
+++ config/JsonConfigParser.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2018-present, Facebook, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "oomd/config/ConfigTypes.h"
+
+namespace Oomd {
+namespace Config2 {
+
+class JsonConfigParser {
+ public:
+  std::unique_ptr<IR::Root> parse(const std::string& input);
+};
+
+} // namespace Config2
+} // namespace Oomd
