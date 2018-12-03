d45af5fb17a40a80dba522846216bf27622487bc
Daniel Xu <dlxu@fb.com>
2018-12-03 14:11:29 -0800

    Implement pressure_rising_beyond
    Summary: This patch reimplements part of the old OomDetector.
    
    Reviewed By: mokomull
    
    Differential Revision: D9688271
    
    fbshipit-source-id: dcceb0a116262adf6902d81fa16fbef2cfe6d3b5

---
+++ plugins/CorePluginsTest.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2018-present, Facebook, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <unordered_map>
+
+#include "oomd/OomdContext.h"
+#include "oomd/PluginRegistry.h"
+#include "oomd/engine/BasePlugin.h"
+
+using namespace Oomd;
+using namespace testing;
+
+namespace {
+std::unique_ptr<Engine::BasePlugin> createPlugin(const std::string& name) {
+  return std::unique_ptr<Engine::BasePlugin>(
+      Oomd::getPluginRegistry().create(name));
+}
+} // namespace
+
+TEST(PresureRisingBeyond, DetectsHighMemPressure) {
+  auto plugin = createPlugin("pressure_rising_beyond");
+  ASSERT_NE(plugin, nullptr);
+
+  Engine::MonitoredResources resources;
+  std::unordered_map<std::string, std::string> args;
+  args["cgroup_fs"] = "oomd/fixtures/plugins/pressure_rising_beyond";
+  args["cgroup"] = "high_pressure";
+  args["resource"] = "memory";
+  args["threshold"] = "80";
+  args["duration"] = "0";
+  args["fast_fall_ratio"] = "0";
+
+  ASSERT_EQ(plugin->init(resources, std::move(args)), 0);
+
+  OomdContext ctx;
+  EXPECT_EQ(plugin->run(ctx), Engine::PluginRet::CONTINUE);
+}
+
+TEST(PresureRisingBeyond, NoDetectLowMemPressure) {
+  auto plugin = createPlugin("pressure_rising_beyond");
+  ASSERT_NE(plugin, nullptr);
+
+  Engine::MonitoredResources resources;
+  std::unordered_map<std::string, std::string> args;
+  args["cgroup_fs"] = "oomd/fixtures/plugins/pressure_rising_beyond";
+  args["cgroup"] = "low_pressure";
+  args["resource"] = "memory";
+  args["threshold"] = "80";
+  args["duration"] = "0";
+  args["fast_fall_ratio"] = "0";
+
+  ASSERT_EQ(plugin->init(resources, std::move(args)), 0);
+
+  OomdContext ctx;
+  EXPECT_EQ(plugin->run(ctx), Engine::PluginRet::STOP);
+}
