66f051df67dd1560225f33b69ab7b37ea84d1dd9
Daniel Xu <dlxu@fb.com>
2019-01-09 18:46:08 -0800

    Print backtrace on abort
    Summary:
    This patch adds backtrace output on aborts to oomd. If we recognize
    we're being compiled by glibc, we can go ahead and use its backtrace symbol
    support. If not, we output nothing.
    
    Differential Revision: D13615533
    
    fbshipit-source-id: 111a4ab8f4626af84d6ac1feac4469ac53d00382

--- include/Assert.cpp
+++ include/Assert.cpp
@@ -15,15 +15,34 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <features.h>
+// backtrace(3) is a GNU extension
+#ifdef __GNU_LIBRARY__
+#include <execinfo.h>
+#include <unistd.h>
+#endif // __GNU_LIBRARY__
+
 #include <cstdlib>
 #include <iostream>
 
 #include "oomd/include/Assert.h"
+
+void __bt() {
+#ifdef __GNU_LIBRARY__
+  static const int num = 100;
+  void* buffer[num];
+  int n = backtrace(buffer, num);
+
+  std::cerr << "Backtrace (most recent call first):" << std::endl;
+  backtrace_symbols_fd(buffer, n, STDERR_FILENO);
+#endif // __GNU_LIBRARY__
+}
 
 [[noreturn]] void
 __OCHECK_FAIL(const char* expr, const char* file, int line, const char* func) {
   std::cerr << file << ":" << line << ": " << func << ": Assertion \'" << expr
             << "\' failed." << std::endl
             << std::flush;
+  __bt();
   std::abort();
 }
