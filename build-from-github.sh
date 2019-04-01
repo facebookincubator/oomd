5227c2a0d107b5b7bd622ecaf1d0bd713a89d99f
Daniel Xu <dxu@dxuuu.xyz>
2019-04-01 12:14:20 -0700

    Bump compiler version (#59)
    Summary:
    We need a newer compiler to use designated assignments for structs.
    The feature technically slated for c++20 but it'd be better to just
    go ahead and depend on that versus hacking together macros to recreate
    the feature.
    Pull Request resolved: https://github.com/facebookincubator/oomd/pull/59
    
    Differential Revision: D14710262
    
    Pulled By: danobi
    
    fbshipit-source-id: c0348f3775ef056f2033142e547455d650e13182

--- build-from-github.sh
+++ build-from-github.sh
@@ -8,6 +8,9 @@ OUTDIR=build
 rm -f $COMPATIBILITY_SYMLINK
 ln -s . $COMPATIBILITY_SYMLINK
 
+# Force usage of new compiler. This is a hack until we use a newer ubuntu
+# image (with newer compiler defaults)
+ln -f -s $(which g++-8) $(which g++)
 
 meson $OUTDIR
 ninja -C $OUTDIR test
