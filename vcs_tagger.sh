b63c664c79062ddba5a3ad6f7c2286fa77ab09d7
Daniel Xu <dxu@dxuuu.xyz>
2019-08-12 16:17:37 -0700

    Address pramga once and bump hash len to 7

--- vcs_tagger.sh
+++ vcs_tagger.sh
@@ -7,5 +7,5 @@ fallback="$2"
 
 # Go into provided dir so builds started from outside the project
 # directory still generate the right tags
-cd "$dir" &>/dev/null && git describe --abbrev=4 --dirty --tags 2>/dev/null ||
+cd "$dir" &>/dev/null && git describe --abbrev=7 --dirty --tags 2>/dev/null ||
   echo "$fallback"
